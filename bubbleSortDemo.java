59c78de9f16fa95432908f87440a4e10a1a2aeef
LeonJasonLu <jason1997.0102@gmail.com>
2023-05-17 17:31:10 +0800

    Update bubbleSortDemo.java

--- bubbleSortDemo.java
+++ bubbleSortDemo.java
@@ -7,11 +7,16 @@ public class bubbleSortDemo{
     }
     public static void bubbleSort(int[] a){
         for(int i = a.length-1; i >= 1; i--){
+            boolean allSorted = true; //增進效能用
             for(int j = 0; j < i; j++){
                 if(a[j] > a[j+1]){
                     swap(a, j, j+1);
+                    allSorted = false; //增進效能用
                 }
             }
+            //增進效能用。若在進行此內層迴圈的過程中完全沒有發生數值交換的情形，
+            //則可以判定陣列中的數值已經整齊排列完畢，因此可以提前脫離
+            if(allSorted == true)break; 
         }
     }
     private static void swap(int[] a, int i, int j){
